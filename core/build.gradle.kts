b58ef62117f2ecd52c42064f7d0adad5a5b6f297
Justin <me@jxtn.de>
2025-03-17 19:02:51 +0100

    add lwjgl as dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,8 +4,7 @@ dependencies {
     labyProcessor()
     api(project(":api"))
 
-    // An example of how to add an external dependency that is used by the addon.
-    // addonMavenDependency("org.jeasy:easy-random:5.0.0")
+    compileOnly("org.lwjgl:lwjgl-glfw:3.3.3")
 }
 
 labyModAnnotationProcessor {

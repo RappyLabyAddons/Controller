5f89de0c5531fd53ca4059da1d33f55914a64f19
RappyTV <rappytv@rappytv.com>
2025-03-21 19:32:35 +0100

    Remove direction keymappings * Make jump binding final

--- core/src/main/java/de/einsjustin/controller/config/KeyMappingConfiguration.java
+++ core/src/main/java/de/einsjustin/controller/config/KeyMappingConfiguration.java
@@ -1,5 +1,6 @@
 package de.einsjustin.controller.config;
 
+import de.einsjustin.controller.api.ControllerKey;
 import de.einsjustin.controller.ui.widgets.ControllerKeyBindWidget.ControllerKeyBindSetting;
 import net.labymod.api.configuration.loader.Config;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
@@ -7,12 +8,6 @@ import net.labymod.api.configuration.loader.property.ConfigProperty;
 public class KeyMappingConfiguration extends Config {
 
   @ControllerKeyBindSetting("key.jump")
-  private ConfigProperty<Integer> jump = new ConfigProperty<>(-1);
-
-  @ControllerKeyBindSetting("key.forward")
-  private ConfigProperty<Integer> forward = new ConfigProperty<>(-1);
-
-  @ControllerKeyBindSetting("key.back")
-  private ConfigProperty<Integer> backward = new ConfigProperty<>(-1);
+  private final ConfigProperty<Integer> jump = new ConfigProperty<>(ControllerKey.A.getButtonId());
 
 }
